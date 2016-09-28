6f6c681da114bba5e30fd738056e041c69f5dbaf
Ibrahim Umar <ibrahim.umar@uit.no>
2016-09-28 16:54:53 +0200

    Updated demo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Simple C program to visualize the output patterns of different hashing functions
 
 ## Usage
 Just run `make` and open the `display.html` file using your favorite browser
+
+## Output demo
+
+See the output in JSFiddle: http://jsfiddle.net/gh/get/mootools/1.6/iambaim/scramble/tree/master/demo
--- demo/demo.js
+++ demo/demo.js
@@ -1,52 +1,54 @@
-var ghpath = "https://raw.githubusercontent.com/iambaim/scramble/master/demo/";
+window.addEvent('domready', function() {
+  var ghpath = "https://raw.githubusercontent.com/iambaim/scramble/master/demo/";
 
-var limit = 20000;
+  var limit = 20000;
 
-var methods = ["fmod", "mult", "none"];
+  var methods = ["fmod", "mult", "none"];
 
-var methodsdetailed = ['#define _HASH_CONSTANT  11400714819323198485ULL</br>#define HASH(X) (X * _HASH_CONSTANT)', "#define HASH(X)         (uint64_t)floor(9223372036854775808ULL * MOD(X))</br>#define MOD(X) fmod((double)X * 0.61803398875, 1)", ""];
+  var methodsdetailed = ['#define _HASH_CONSTANT  11400714819323198485ULL</br>#define HASH(X) (X * _HASH_CONSTANT)', "#define HASH(X)         (uint64_t)floor(9223372036854775808ULL * MOD(X))</br>#define MOD(X) fmod((double)X * 0.61803398875, 1)", ""];
 
-var texts = [];
-texts["seq"] = "Sequential";
-texts["rand"] = "Random";
+  var texts = [];
+  texts["seq"] = "Sequential";
+  texts["rand"] = "Random";
 
-var place = [];
+  var place = [];
 
-for (var k in texts) {
+  for (var k in texts) {
 
-	var inputtype = texts[k];
+    var inputtype = texts[k];
 
-	document.getElementById('content').innerHTML += "<h2>" + inputtype + " input</h2>";
+    document.getElementById('content').innerHTML += "<h2>" + inputtype + " input</h2>";
 
 
-	for (var l in methods) {
+    for (var l in methods) {
 
-		var lp = parseInt(l) + 1;
+      var lp = parseInt(l) + 1;
 
-		document.getElementById('content').innerHTML += "<h3>Hashing method: " + methods[l] + "</h3>";
-		document.getElementById('content').innerHTML += "<div><code>" + methodsdetailed[l] + "</code></div><br/>";
-		document.getElementById('content').innerHTML += '<div class="def" id="' + k + lp.toString() + '"></div><br/>';
+      document.getElementById('content').innerHTML += "<h3>Hashing method: " + methods[l] + "</h3>";
+      document.getElementById('content').innerHTML += "<div><code>" + methodsdetailed[l] + "</code></div><br/>";
+      document.getElementById('content').innerHTML += '<div class="def" id="' + k + lp.toString() + '"></div><br/>';
 
-	}
-}
+    }
+  }
 
-for (var k in texts) {
+  for (var k in texts) {
 
-	var inputtype = texts[k];
+    var inputtype = texts[k];
 
-	for (var l in methods) {
+    for (var l in methods) {
 
-		var lp = parseInt(l) + 1;
+      var lp = parseInt(l) + 1;
 
-		place[k + lp.toString()] = new Dygraph(
-			document.getElementById(k + lp.toString()),
-			ghpath + k + "." + methods[l] + "." + limit + ".out", // path to CSV file
-			{
-				title: inputtype + " with range: 0-" + limit,
-				drawPoints: true,
-				strokeWidth: 0,
-				pointSize: 2,
-			}
-		);
-	}
-}
+      place[k + lp.toString()] = new Dygraph(
+        document.getElementById(k + lp.toString()),
+        ghpath + k + "." + methods[l] + "." + limit + ".out", // path to CSV file
+        {
+          title: inputtype + " with range: 0-" + limit,
+          drawPoints: true,
+          strokeWidth: 0,
+          pointSize: 2,
+        }
+      );
+    }
+  }
+})
